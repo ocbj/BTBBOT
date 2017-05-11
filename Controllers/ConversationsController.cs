197f42fa8d7d3e212fb23db131e09357590e324d
Billy O'Connor <billy.oconnor@factset.com>
2017-05-11 16:13:17 +0100

    Can create conversations

--- Controllers/ConversationsController.cs
+++ Controllers/ConversationsController.cs
@@ -1,7 +1,6 @@
-﻿using LuisBot.Dialogs;
-using Microsoft.Bot.Builder.Dialogs;
-using Microsoft.Bot.Connector;
+﻿using Microsoft.Bot.Connector;
 using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -9,8 +8,12 @@ using System.Web.Http;
 
 namespace LuisBot.Controllers
 {
+    /// <summary>
+    /// Initiates a conversation with the bot
+    /// </summary>
     public class ConversationsController : ApiController
     {
+
         /// <summary>
         /// Get api/conversations/{username}
         /// </summary>
@@ -18,15 +21,50 @@ namespace LuisBot.Controllers
         /// <returns></returns>
         public async Task<HttpResponseMessage> Get([FromUri]string username)
         {
-            var newMessage = Activity.CreateMessageActivity();
-            newMessage.From = new ChannelAccount("botId", "botName");
-            newMessage.Recipient = new ChannelAccount("email", "name");
-            newMessage.ServiceUrl = "http://BrokenBuildBot/Uri/App";
-            newMessage.Text = "Hello World";
-            var connector = new ConnectorClient(new Uri("http://BrokenBuildBot/Uri/App"), "AppId", "AppPassword");
-            var conversation = connector.Conversations.CreateDirectConversation(newMessage.From, newMessage.Recipient);
-            newMessage.Conversation.Id = conversation.Id;
-            await connector.Conversations.SendToConversationAsync((Activity)newMessage);
+            try
+            {
+                var newMessage = Activity.CreateMessageActivity();
+                newMessage.From = new ChannelAccount("177b2eem05dc4542ac", "Bot");
+                newMessage.Recipient = new ChannelAccount("default-user", "default-user");
+                newMessage.ServiceUrl = "http://localhost:3979/api/messages";
+                newMessage.Text = $"Hello {username}";
+
+                HeroCard heroCard = new HeroCard()
+                {
+                    Title = "You broke the build bro?",
+                    Subtitle = $"I think you broke the build bro",
+                    Images = new List<CardImage>()
+                        {
+                            new CardImage() { Url = "https://placeholdit.imgix.net/~text?txtsize=33&txt=350%C3%97150&w=350&h=150" }
+                            //    },
+                            //Buttons = new List<CardAction>()
+                            //    {
+                            //        new CardAction()
+                            //        {
+                            //            Title = "More details",
+                            //            Type = ActionTypes.OpenUrl,
+                            //            Value = $"https://www.bing.com/search?q=hotels+in+" + HttpUtility.UrlEncode(hotel.Location)
+                            //        }
+                            //    }
+                    }
+                };
+
+                newMessage.Attachments.Add(heroCard.ToAttachment());
+
+
+
+                var connector = new ConnectorClient(new Uri("http://localhost:54424"), "", "");
+                var conversations = connector.Conversations;
+                var conversation = conversations.CreateDirectConversation(newMessage.From, newMessage.Recipient);
+                newMessage.Conversation = new ConversationAccount(id: conversation.Id);
+
+                newMessage.Conversation.Id = conversation.Id;
+                await connector.Conversations.SendToConversationAsync((Activity)newMessage);
+            }
+            catch(Exception e)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, e.InnerException?.Message ?? e.Message); 
+            }
 
             var response = Request.CreateResponse(HttpStatusCode.OK);
             return response;
