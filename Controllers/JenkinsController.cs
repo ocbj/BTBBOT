1c92ef7ae7bb75f7fd8be38a5c1110970b9c065f
Billy O'Connor <billy.oconnor@factset.com>
2017-05-12 05:19:11 +0100

    asd

--- Controllers/JenkinsController.cs
+++ Controllers/JenkinsController.cs
@@ -63,77 +63,14 @@ namespace LuisBot.Controllers
         Random r = new Random();
 
 
-        /// <summary>
-        /// First message that gets sent, with appropriate links
-        /// </summary>
-        /// <param name="username"></param>
-        /// <returns></returns>
-        public IMessageActivity WelcomeMessage(ConversationAccount conversation, string username)
-        {
-            var msg = Activity.CreateMessageActivity();
-            msg.From = new ChannelAccount(botId, "BTBBot");
-            msg.Recipient = new ChannelAccount(username, username);
-            msg.Conversation = conversation;
-            msg.ServiceUrl = svcUrl;
 
-            HeroCard heroCard = new HeroCard()
-            {
-                Title = "Someone broke the build.",
-                Subtitle = $"Was it you {username}?",
-                Images = new List<CardImage>() { new CardImage() { Url = "https://uploads.toptal.io/blog/image/92347/toptal-blog-image-1460406405672-52ec53e6624f51828dab1aee43efe75a.jpg" } },
-                Buttons = new List<CardAction>()
-                {
-                    new CardAction()
-                    {
-                        Title = "Show in JIRA",
-                        Type = ActionTypes.OpenUrl,
-                        Value = $"https://jira.vermilionreporting.com/browse/" + HttpUtility.UrlEncode("VP-319")
-                    },
-                    new CardAction()
-                    {
-                        Title = "Show in Jenkins",
-                        Type = ActionTypes.OpenUrl,
-                        Value = $"https://jira.vermilionreporting.com/browse/" + HttpUtility.UrlEncode("VP-319")
-                    }
-                }
-            };
-
-            msg.Attachments.Add(heroCard.ToAttachment());
-
-            return msg;
-        }
-
-
-        public IMessageActivity CreateMessage(ConversationAccount conversation, string username, string text)
-        {
-            var msg = Activity.CreateMessageActivity();
-            msg.From = new ChannelAccount(botId, "Bot");
-            msg.Recipient = new ChannelAccount(username, username);
-            msg.Conversation = conversation;
-            msg.ServiceUrl = svcUrl;
-            msg.Text = text;
-
-            return msg;
-        }
 
 
         public async Task<HttpResponseMessage> Post([FromBody]BrokeBuildJson data)
         {
             try
             {
-                var user = DataDump.Users[r.Next(DataDump.Users.Count())];
-
-                var changeLog = data.changelog
-                    .Select(x => new CardAction(type: ActionTypes.OpenUrl, title: $"author {x.author} revision {x.revision}", value: ""));
-
-                HeroCard heroCard = new HeroCard()
-                {
-                    Title = $"Someone broke the build #{data.buildNumber}.",
-                    Subtitle = $"Was it you {user.FirstName}?",
-                    Images = new List<CardImage>() { new CardImage() { Url = "https://uploads.toptal.io/blog/image/92347/toptal-blog-image-1460406405672-52ec53e6624f51828dab1aee43efe75a.jpg" } },
-                    Buttons = changeLog.ToList()
-                };
-
+                var user = new User() { SkypeName = "ucariouk", FirstName =  "Bill" }; //  DataDump.Users[r.Next(DataDump.Users.Count())];
 
                 var from = new ChannelAccount(botId, "Bot");
                 var recipient = new ChannelAccount(user.SkypeName, user.FirstName);
@@ -141,9 +78,33 @@ namespace LuisBot.Controllers
                 var conversations = connector.Conversations;
                 var conversation = conversations.CreateDirectConversation(from, recipient);
 
-                var newMessage = WelcomeMessage(new ConversationAccount(id: "8:" + conversation.Id), user.SkypeName);
+                // create msg
+                var msg = Activity.CreateMessageActivity();
+                msg.From = new ChannelAccount(botId, "BTBBot");
+                msg.Recipient = new ChannelAccount(user.SkypeName, user.FirstName);
+                msg.Conversation = new ConversationAccount(id: "8:" + conversation.Id);
+                msg.ServiceUrl = svcUrl;
 
-                await connector.Conversations.SendToConversationAsync((Activity)newMessage);
+                var changeLog = data.changelog.OrderByDescending(x => x.date)
+                    .Select(x => new CardAction()
+                    {
+                        Title = $"{x.author} revision {x.revision}",
+                        Type = ActionTypes.OpenUrl,
+                        Value = $"https://jira.vermilionreporting.com/browse/" + HttpUtility.UrlEncode("VP-319")
+                    });
+
+                HeroCard heroCard = new HeroCard()
+                {
+                    Title = $"Someone broke build #{data.buildNumber}.",
+                    Subtitle = $"Was it you {user.FirstName}?",
+                    Images = new List<CardImage>() { new CardImage() { Url = "http://btbbot.azurewebsites.net/wow.jpg" } },
+                    Buttons = changeLog.ToList()
+                };
+
+                msg.Attachments.Add(heroCard.ToAttachment());
+
+                await connector.Conversations.SendToConversationAsync((Activity)msg);
+
             }
             catch (Exception e)
             {
